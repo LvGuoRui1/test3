8434be3bc7a02901d93dc922d4549f66e69fa27e
lgr1 <lgr1@qq.com>
2022-08-10 08:29:54 +0800

    hox-fix-commit3

--- src/main/java/com/lv/LvJava.java
+++ src/main/java/com/lv/LvJava.java
@@ -4,5 +4,6 @@ public class LvJava {
     public static void main(String[] args) {
         System.out.println("哈哈嘿");
         System.out.println("hot-fix-2");
+        System.out.println("hot-fix-3");
     }
 }
